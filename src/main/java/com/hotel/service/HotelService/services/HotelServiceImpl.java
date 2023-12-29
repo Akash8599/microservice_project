4f87bedcc52af88d4f90ddff0df99fe76ffd1e25
Lenovo <akashkamble8599@gmail.com>
2023-12-29 23:01:36 +0530

    Added loggers

--- src/main/java/com/hotel/service/HotelService/services/HotelServiceImpl.java
+++ src/main/java/com/hotel/service/HotelService/services/HotelServiceImpl.java
@@ -3,6 +3,8 @@ package com.hotel.service.HotelService.services;
 import com.hotel.service.HotelService.entities.Hotel;
 import com.hotel.service.HotelService.exceptions.ResourceNotFoundException;
 import com.hotel.service.HotelService.repositories.HotelRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +12,8 @@ import java.util.List;
 
 @Service
 public class HotelServiceImpl implements HotelService{
+
+    Logger logger = LoggerFactory.getLogger(HotelServiceImpl.class);
 
     @Autowired
     private HotelRepository hotelRepository;
@@ -27,6 +31,7 @@ public class HotelServiceImpl implements HotelService{
 
     @Override
     public Hotel getHotel(String id) {
+        logger.info("Retrieved hotel for given hotel id : {}", id);
         return hotelRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Data not found"));
     }
 }
