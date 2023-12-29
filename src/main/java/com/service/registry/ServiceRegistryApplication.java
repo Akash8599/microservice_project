b2036dd0eae6f7667eadaf6fe1c7e04edcdf8ed7
Lenovo <akashkamble8599@gmail.com>
2023-12-29 10:51:57 +0530

    Added comments

--- src/main/java/com/service/registry/ServiceRegistryApplication.java
+++ src/main/java/com/service/registry/ServiceRegistryApplication.java
@@ -4,6 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+/**
+ * Java code for a Service Registry Application.
+ * This code represents a Spring Boot application that serves as a Eureka Server.
+ * The @SpringBootApplication annotation indicates that this class is the main entry point of the application.
+ * The @EnableEurekaServer annotation enables the Eureka Server functionality.
+ */
+
 @SpringBootApplication
 @EnableEurekaServer
 public class ServiceRegistryApplication {
